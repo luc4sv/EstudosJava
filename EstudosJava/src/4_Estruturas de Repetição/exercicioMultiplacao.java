4f5cd91f5559666180687a20a75dd0580846474e
Lucas Vinícius <74675742+luc4sv@users.noreply.github.com>
2025-04-11 20:11:43 -0300

    Update exercicioMultiplacao.java

--- EstudosJava/src/4_Estruturas de Repetição/exercicioMultiplacao.java
+++ EstudosJava/src/4_Estruturas de Repetição/exercicioMultiplacao.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class exercicioMultiplacao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int produto = 1; // Inicializa com 1 para não afetar a multiplicação
+        int produto = 1;
 
         for (int i = 1; i <= 5; i++) {
             System.out.print("Digite um número: ");
             int num = scanner.nextInt();
-            produto *= num; // Multiplica o número digitado ao produto acumulado
+            produto *= num;
         }
 
         System.out.println("O resultado da multiplicação é: " + produto);
