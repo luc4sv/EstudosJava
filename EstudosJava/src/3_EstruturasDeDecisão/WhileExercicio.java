318369169f5490e253f4057ad60bdf1138bdd13d
Lucas Vinícius <74675742+luc4sv@users.noreply.github.com>
2025-04-11 20:08:48 -0300

    Update WhileExercicio.java

--- EstudosJava/src/3_EstruturasDeDecisão/WhileExercicio.java
+++ EstudosJava/src/3_EstruturasDeDecisão/WhileExercicio.java
@@ -4,13 +4,13 @@ public class WhileExercicio {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
 
-        int i = 1, soma = 0, num; // Inicializando as variáveis corretamente
+        int i = 1, soma = 0, num;
 
         while (i <= 5) {
             System.out.print("Digite um número inteiro para a soma: ");
             num = leia.nextInt();
-            soma = soma + num; // Acumulando a soma
-            i = i + 1; // Incrementando i corretamente
+            soma = soma + num; 
+            i = i + 1;
         }
 
         System.out.println("Soma total: " + soma);
